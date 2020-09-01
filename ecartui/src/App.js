a99495491829b2744319c7981b01bae9c3218fa2
Ronipaul99 <66959326+Ronipaul99@users.noreply.github.com>
2020-09-01 21:06:37 +0530

    navbar basic

--- ecartui/src/App.js
+++ ecartui/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login';
-
+import Appbar from './components/navbar'
+import Example from './components/example'
 function App() {
   return (
     
       <div>
-             <Login/>
+        <Appbar/>
+        <Login/>
        </div>
   );
 }
